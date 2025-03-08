bc49d0cd6cbb51ed4a6fb0d43598f414975c95d7
Nikhil Jatale <nikhiljatale@gmail.com>
2025-03-08 02:40:48 +0000

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Snehadi_photography 
-### A Baby photography website
-checkout [Snehadi_photography](https://njtales.github.io/Snehadi_photography/)
--- backend-sanity/schemas/index.js
+++
@@ -1,5 +0,0 @@
-import Home from './Home'
-import Testimonials from './Testimonials'
-import Work from './Work'
-import PortfolioCategory from './PortfolioCategory'
-export const schemaTypes = [Home, PortfolioCategory, Testimonials, Work]
