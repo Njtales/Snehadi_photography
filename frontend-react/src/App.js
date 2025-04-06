a3a45d7c7b308490bd0a698a262584e678803c2e
Nikhil Jatale <nikhiljatale@gmail.com>
2025-04-06 19:34:43 +0100

    pusshing on git page

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './container/Home/Home'; 
 import Portfolio from './container/Portfolio/Portfolio'; 
 import WhoIam from './container/WhoIam/WhoIam';
